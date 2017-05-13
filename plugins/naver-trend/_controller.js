4f4d67ce65ecd3e333b1e99974641e549aa5f811
ParkJongWoo <jjong802@gmail.com>
2017-05-13 16:51:08 +0900

    add Naver trend

--- plugins/naver-trend/_controller.js
+++ plugins/naver-trend/_controller.js
@@ -2,42 +2,45 @@ function NaverTrend($scope, $http, $q, $interval) {
 
 	$scope.NT_currentIndex = 0;
 	var trends = {};
+	var trendsRes = {};
 	trends.feed = [];
-	trends.title = '실시간 검색어 순위';
-	$scope.lineCount = config.naver_trend.lineCount;
 
-	var refreshTrends = function () {
+	trends.get = function () {
 		var tr = require('naver-trends');
-		$scope.howWord = null;
-		trends.feed = [];
 
 		trends.updated = new moment().format('MMM DD, h:mm a');
 
 		tr.load(['real'], function (err, result) {			
-			//console.log(err, JSON.stringify(result));
+			  console.log(err, JSON.stringify(result))
+			  return trendsRes.result = result;
+		})
+	};
 
-			console.log(result.real.length);
-			//For each feed
-			for (var i = 0; i < result.real.length - 10; i++) {
+	var refreshTrends = function () {
+		$scope.howWord = null;
+		trends.get().then(function (response) {
+			console.log(trendsRes.result.real.length);
+            //For each feed
+			for (var i = 0; i < trendsRes.result.real.length - 10; i++) {
 					var feedEntry = {
-						title: i+1 + ". "+result.real[i].title,
+						title: trendsRes.result.real[1].title,
                         //content: response[i].data.query.results.rss.channel.item[j].description[0],
 					};
 					trends.feed.push(feedEntry);
 			}
 			$scope.NT_currentIndex = 0;
 			$scope.trends  = trends;
-		})
+		});
 	};
 
 	var cycleTrends = function(){
-		$scope.NT_currentIndex = (($scope.NT_currentIndex + config.naver_trend.lineCount) >= $scope.trends.feed.length)? 0: $scope.NT_currentIndex + config.naver_trend.lineCount;
+		$scope.NT_currentIndex = ($scope.NT_currentIndex >= $scope.trends.feed.length)? 0: $scope.NT_currentIndex + 1;
 	}
 
 	if (typeof config.naver_trend !== 'undefined' && typeof config.naver_trend.refreshInterval != 'undefined') {
 		refreshTrends();
 		$interval(refreshTrends, config.naver_trend.refreshInterval * 60000 || 1800000)
-		$interval(cycleTrends, 6000)
+		$interval(cycleTrends, 8000)
 	}
 }
 
